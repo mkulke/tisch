5d8b016ca4e40fd3c369abae4be95c33aa2a4339
Magnus Kulke <mkulke@gmail.com>
2014-09-27 00:10:37 +0200

    Added update task api.

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,6 +4,7 @@ var _ = require('underscore')._;
 var db = require('../lib/postgres.js');
 var respondWithError = require('../lib/utils.js').respondWithError;
 var respondWithResult = require('../lib/utils.js').respondWithResult;
+var constants = require('../lib/constants.json');
 
 router.route('/task').get(function(req, res) {
 	db.findTasks()
@@ -19,6 +20,12 @@ router.route('/task/:id').get(function(req, res) {
 
 router.route('/task/:id').post(function(req, res) {
 	db.updateTask(req.params.id, req.get('Rev'), req.get('Key'), req.get('Value'), req.get('Index'))
+	.then(_.partial(respondWithResult, res))
+	.fail(_.partial(respondWithError, res));
+});
+
+router.route('/task').post(function(req, res) {
+	db.addTask(_.extend(constants.templates.task, {story_id: req.get('Parent-Id')}))
 	.then(_.partial(respondWithResult, res))
 	.fail(_.partial(respondWithError, res));
 });
