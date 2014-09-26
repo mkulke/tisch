456ec77b84a829bca1e0a2124c7a66025f8c05f5
Magnus Kulke <mkulke@gmail.com>
2014-09-26 23:52:09 +0200

    Added update to task api.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var tasks = require('./routes/tasks');
+var stories = require('./routes/stories');
+var sprints = require('./routes/sprints');
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/api', tasks);
+app.use('/api', stories);
+app.use('/api', sprints);
 
 module.exports = app;
--- routes/stories.js
+++ routes/stories.js
@@ -5,20 +5,14 @@ var db = require('../lib/postgres.js');
 var respondWithError = require('../lib/utils.js').respondWithError;
 var respondWithResult = require('../lib/utils.js').respondWithResult;
 
-router.route('/task').get(function(req, res) {
-	db.findTasks()
+router.route('/story').get(function(req, res) {
+	db.findStories()
 	.then(_.partial(respondWithResult, res))
 	.fail(_.partial(respondWithError, res));
 });
 
-router.route('/task/:id').get(function(req, res) {
-	db.findSingleTask(req.params.id)
-	.then(_.partial(respondWithResult, res))
-	.fail(_.partial(respondWithError, res));
-});
-
-router.route('/task/:id').post(function(req, res) {
-	db.updateTask(req.params.id, req.get('Rev'), req.get('Key'), req.get('Value'), req.get('Index'))
+router.route('/story/:id').get(function(req, res) {
+	db.findSingleStory(req.params.id)
 	.then(_.partial(respondWithResult, res))
 	.fail(_.partial(respondWithError, res));
 });
