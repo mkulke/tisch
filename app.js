79c405ecda7f68feae109c3479680f0f24e473af
Magnus Kulke <mkulke@gmail.com>
2014-09-29 23:41:30 +0200

    Initial tests w/ bootstrap.

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ var stories = require('./routes/stories');
 var sprints = require('./routes/sprints');
 var app = express();
 
+app.set('view engine', 'jade');
+
+app.use(express.static(__dirname + '/public/vendor'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.use('/api', tasks);
 app.use('/api', stories);
 app.use('/api', sprints);
+
+app.get('/app', function(req, res){
+  res.render('task', {test: 'My string'});
+});
+
 
 module.exports = app;
