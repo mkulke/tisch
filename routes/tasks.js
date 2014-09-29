e646fce2fdb8be7535abad8e8bd488a7bc68b16b
Magnus Kulke <mkulke@gmail.com>
2014-09-29 22:08:48 +0200

    Added links to all task verbs/actions.

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var _ = require('underscore')._;
 var db = require('../lib/postgres.js');
+var Q = require('q');
 var respondWithError = require('../lib/utils.js').respondWithError;
 var respondWithResult = require('../lib/utils.js').respondWithResult;
 var constants = require('../lib/constants.json');
@@ -15,6 +16,12 @@ var addStoryLink = function(req, result) {
 	});
 
 	return result;
+};
+
+var process = function(req, res, result) {
+	return Q.fcall(_.partial(addSelfLink, req, result))
+	.then(_.partial(addStoryLink, req))
+	.then(_.partial(respondWithResult, res));
 };
 
 router.route('/task').get(function(req, res) {
@@ -31,33 +38,31 @@ router.route('/task').get(function(req, res) {
 
 router.route('/task/:id').get(function(req, res) {
 	db.findSingleTask(req.params.id)
-	.then(_.partial(addSelfLink, req))
-	.then(_.partial(addStoryLink, req))
-	.then(_.partial(respondWithResult, res))
+	.then(_.partial(process, req, res))
 	.fail(_.partial(respondWithError, res));
 });
 
 router.route('/task/:id/:property').put(function(req, res) {
 	db.updateTask(req.params.id, req.get('Rev'), req.params.property, req.body.value)
-	.then(_.partial(respondWithResult, res))
+	.then(_.partial(process, req, res))
 	.fail(_.partial(respondWithError, res));
 });
 
 router.route('/task/:id/:property/:index').put(function(req, res) {
 	db.updateIndexedTaskProperty(req.params.id, req.get('Rev'), req.params.property, req.params.index, req.body.value)
-	.then(_.partial(respondWithResult, res))
+	.then(_.partial(process, req, res))
 	.fail(_.partial(respondWithError, res));
 });
 
 router.route('/task').post(function(req, res) {
 	db.addTask(_.extend(constants.templates.task, {story_id: req.get('Parent-Id')}))
-	.then(_.partial(respondWithResult, res))
+	.then(_.partial(process, req, res))
 	.fail(_.partial(respondWithError, res));
 });
 
 router.route('/task/:id').delete(function(req, res) {
 	db.removeTask(req.params.id, req.get('Rev'))
-	.then(_.partial(respondWithResult, res))
+	.then(_.partial(process, req, res))
 	.fail(_.partial(respondWithError, res));
 });
 
