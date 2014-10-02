44e7a8216daaadcdca54fcb167e0d044a2a981b7
Magnus Kulke <mkulke@gmail.com>
2014-10-02 09:58:59 +0200

    Enhanced ajax error handling.

--- public/js/task.js
+++ public/js/task.js
@@ -11,14 +11,32 @@ var model = (function () {
     $.ajax({
       type: 'PUT',
       headers: {Rev: rev},
-      url: '/api/task/' + id + '/' + property,
+      url: '/api/tasik/' + id + '/' + property,
       contentType: "application/json",
       data: JSON.stringify({value: value})
     })
     .done(doneFn)
     .fail(function (jqXHR, textStatus, errorThrown) {
+      var message;
+
+      if (jqXHR.status === 0) {
+        message = 'Could not connect to backend. Verify network.';
+      } else if (jqXHR.status == 404) {
+        message = 'Requested resource not found.';
+      } else if (jqXHR.status == 500) {
+        message = jqXHR.responseText + '.';
+      } else if (errorThrown === 'parsererror') {
+        message = 'JSON parsing error';
+      } else if (errorThrown === 'timeout') {
+        message = 'Connection to backend timed out.';
+      } else if (exception === 'abort') {
+        message = 'The backend transaction has been aborted by the client';
+      } else {
+        message = 'Uncaught Error (' + jqXHR.responseText + ').';
+      }
+
       viewModel.error.title('Problem');
-      viewModel.error.message('Could not update the task on the backend: ' + jqXHR.responseText + '.');
+      viewModel.error.message(message);
       $('#error-modal').modal();
     });
   };
