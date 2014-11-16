a0253408f746c8061456c056cc7c73e4f9ceb5ce
Magnus Kulke <mkulke@gmail.com>
2014-11-16 15:00:03 +0100

    Further work on time-spent component.

--- public/js/components/time-spent.js
+++ public/js/components/time-spent.js
@@ -1,8 +1,30 @@
 ko.components.register('time-spent', {
     viewModel: function(params) {
-      $('#time-spent').datepicker({
+      $('#time-spent-index').datepicker({
         format: "yyyy-mm-dd"
       });
+
+      ko.extenders.requiresMatch = function(target, regex) {
+        //add some sub-observables to our observable
+        target.hasError = ko.observable();
+        target.validationMessage = ko.observable();
+
+        //define a function to do validation
+        function validate(newValue) {
+           target.hasError(!regex.test(newValue));
+        }
+
+        //initial validation
+        validate(target());
+
+        //validate whenever the value changes
+        target.subscribe(validate);
+
+        //return the original observable
+        return target;
+      };
+
+      this.timeSpentValue = ko.observable("3h").extend({requiresMatch: /^\d+h$/});
     },
     template: {
       fromUrl: 'time-spent'
